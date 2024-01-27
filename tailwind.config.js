21d638245fbeb7d924169c166e277e71c36a1da1
thisismeZML <thatismetheoneudunknow@gmail.com>
2024-01-27 14:07:49 +0630

    Adding Images

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        PrimaryColor: "#08ac0a",
+        SecondaryColor: "#198753",
+        ThirdColor: "#011e2b",
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        default: "1rem",
+        sm:"2rem"
+      }
+    }
   },
   plugins: [],
 };
